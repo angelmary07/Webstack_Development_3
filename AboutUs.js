a6a51a12ab638993fe5f147d7f19dee440a19ebb
Angel Mary Sony <angelmary.sony@bcah.christuniversity.in>
2026-01-21 23:30:22 +0530

    Add AboutUs component with introductory content

---
+++ AboutUs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AboutUs() {
+  return (
+    <div className="card" style={{ maxWidth: '800px' }}>
+      <h1>About Us</h1>
+      <p>Elite Resident Community is a modern residential society committed to providing a safe, comfortable, and vibrant living experience.</p>
+    </div>
+  );
+}
+
+export default AboutUs;
