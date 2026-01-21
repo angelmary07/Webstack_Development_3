9333c3dc23028f9dab4c22cb3e4a15146e81f1e8
Angel Mary Sony <angelmary.sony@bcah.christuniversity.in>
2026-01-21 23:27:19 +0530

    Add Footer component with copyright notice

---
+++ Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>© 2025 Elite Resident Community — All Rights Reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
